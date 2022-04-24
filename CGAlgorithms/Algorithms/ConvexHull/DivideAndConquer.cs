8540abe2959a7c4f8a95e7f36d93f9cda6f4caf0
Israa Ezzat <62281743+israa639@users.noreply.github.com>
2022-04-24 13:34:09 -0700

    d&c

--- CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
+++ CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
@@ -21,33 +21,33 @@ namespace CGAlgorithms.Algorithms.ConvexHull
             }
             return outPoints;
         }
-        //public List<Point> mergeHulls(List<Point> left_hull, List<Point> right_hull)
-        //{
+        public List<Point> mergeHulls(List<Point> left_hull, List<Point> right_hull)
+        {
 
-        //}
-        //public List<Point> divide(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
-        //{
+        }
+        public List<Point> divide(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
+        {
 
-        //    if (points.Count < 6)
-        //    {
-        //        JarvisMarch jv = new JarvisMarch();
-        //        jv.Run(points, lines, polygons, ref outPoints, ref outLines, ref outPolygons);
-        //        return outPoints;
-        //    }
-        //    int mid = (points.Count / 2) - 1;
+            if (points.Count < 6)
+            {
+                JarvisMarch jv = new JarvisMarch();
+                jv.Run(points, lines, polygons, ref outPoints, ref outLines, ref outPolygons);
+                return outPoints;
+            }
+            int mid = (points.Count / 2) - 1;
 
-        //    List<Point> lch = getPoints(points, 0, mid);
-        //    List<Point> rch = getPoints(points, mid + 1, points.Count - 1);
-        //    List<Point> left_hull = divide(lch, lines, polygons, ref outPoints, ref outLines, ref outPolygons);
-        //    List<Point> right_hull = divide(rch, lines, polygons, ref outPoints, ref outLines, ref outPolygons);
+            List<Point> lch = getPoints(points, 0, mid);
+            List<Point> rch = getPoints(points, mid + 1, points.Count - 1);
+            List<Point> left_hull = divide(lch, lines, polygons, ref outPoints, ref outLines, ref outPolygons);
+            List<Point> right_hull = divide(rch, lines, polygons, ref outPoints, ref outLines, ref outPolygons);
 
-        //    return mergeHulls(left_hull, right_hull);
+            return mergeHulls(left_hull, right_hull);
 
-        //}
+        }
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
             HelperMethods.filterPoints(points);
-            List<Point> sortedList = points.OrderBy(x => x.X).ThenBy(y => y.Y).ToList();
+            List<Point> sortedList = points.OrderBy(x => x.X).ToList();//.ThenBy(y => y.Y).ToList();
 
 
         }
